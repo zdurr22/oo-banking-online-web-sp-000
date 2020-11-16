ec0aadc3ea441df0378c9208487761f56c6cf86c
Zachary Durr <zdurr22@gmail.com>
2020-11-16 20:41:10 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,6 +17,8 @@ class Transfer
   def execute_transaction
     @sender.balance = @sender.balance - @amount
     @receiver.balance = @receiver.balance + @amount
+    @status = "complete"
+    
   end
 
 

ec2661faa1e3c2ecdc7ec7ab06bbc2c9937b1deb
Zachary Durr <zdurr22@gmail.com>
2020-11-16 20:50:46 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,10 +16,11 @@ class Transfer
   end
 
   def execute_transaction
+    if @sender.balance > @amount && @status == "pending"
       @sender.balance = @sender.balance - @amount unless @sender.balance < @amount
       @receiver.balance = @receiver.balance + @amount unless @sender.balance < @amount
       @status = "complete" unless @sender.balance < @amount
-    if @sender.balance < @amount
+
       return "Transaction rejected. Please check your account balance."
     end
   end

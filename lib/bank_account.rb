598c01c0005ac5a2308a367c9da9ec42cf4a3bc1
Zachary Durr <zdurr22@gmail.com>
2020-11-16 20:26:28 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,7 +1,7 @@
 class BankAccount
   attr_accessor :balance, :status
   attr_reader :name
-  
+
   def initialize(name)
     @name = name
     @balance = 1000
@@ -11,7 +11,7 @@ class BankAccount
   def deposit(amount)
     @balance = @balance + amount
   end
-    
 
-  
+
+
 end

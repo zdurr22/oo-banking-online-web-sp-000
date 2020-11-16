d1cfae3aab2c2814649021de25ee56afcf72287f
Zachary Durr <zdurr22@gmail.com>
2020-11-16 20:53:34 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.balance > @amount && @status == "pending"
+    if @sender.balance > @amount && @status == "pending" && @sender.status != "closed"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
